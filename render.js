ebc9820aef3cc7a3258aa02147261cef229ca858
MajoretteKay <keolecki@gmail.com>
2019-12-04 14:58:42 -0500

    sign up page basic

--- render.js
+++ render.js
@@ -3,36 +3,63 @@
 export function signInForm() {
     // renders the sign in form
     return `<div id="signin">
+    <h> Sign In </h>
     <label>Don't Have An Account?</label>
-    <button>Sign Up</button>
-    <form id="signInForm">
-    <label>Userame</label>
+    <button class="signUpButton">Sign Up</button>
+    <form class="signInForm">
+    <label>Userame:</label>
     <input></input>
-    <label>Password</label>
-    <input></input>
+    <label>Password:</label>
+    <input type="password"></input>
     <button type="submit">Sign In</button>
     </form> 
     <div>`;
 }
 
 export async function signInHandler() {
- //cant be blank
+ //handler for submit button using axios to confirm 
+ //or deny user access, if WithCredentials true then 
+ //grant access to main website.
+
+    
 }
 
 export async function signUpForm() {
     // renders the sign up form
-    $('#signin').replaceWith(`
-    `);
+    $('#signin').replaceWith(`<div id="signup">
+    <h> Sign Up </h>
+    <form class="">
+    <label>Username:</label>
+    <input></input>
+    <label>Password:</label>
+    <input></input>
+    <label>Birthday:</label>
+    <input></input>
+    <label>First Name:</label>
+    <input></input>
+    <label>Last Name:</label>
+    <input></input>
+    </form>
+    </div>`);
+}
+
+export async function signUpHandler() {
+    //handler for submit button
+}
+
+export async function signInSwitch() {
+    // switches the sign up form with sign in
+    $('#signin').replaceWith(signInForm());
 }
 
 export async function renderSite() {
     const $root = $('#root');
     $root.append(signInForm());
 
-    $root.on("submit", "#signInForm", signInHandler);
-    $root.on("click", "#signupForm", signUpForm);
-    $root.on("submit", "", signInHandler);
-    $root.on("click", "", signInForm);
+    $root.on("submit", ".signInForm", signInHandler);
+    $root.on("click", ".signUpButton", signUpForm);
+    $root.on("submit", ".signupForm", signUpHandler);
+    $root.on("click", ".signInButton", signInSwitch);
 
 }
 
--- user.js
+++ user.js
@@ -2,7 +2,6 @@ const User = class { // basic outline of user object
     constructor(name, user, email, password, birthday) {
         this.name = name;
         this.username = user; //unique name for people, cannot match any others
-        this.email = email;
         this.password = password;
         this.birthday = birthday; // dealt with the same way as date, first event added to calendar.
     }
