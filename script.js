fe59e65d8488e88ee706e96850dd1109d40c0b55
MajoretteKay <keolecki@gmail.com>
2019-12-08 12:29:50 -0500

    API 3 days

--- script.js
+++ script.js
@@ -78,7 +78,7 @@ export function renderCal() {
         calendar+=`</tr>`;
     }
     calendar += `</table>`;
-    calendar += `<a class="weatherwidget-io" href="https://forecast7.com/en/35d91n79d06/chapel-hill/?unit=us" data-label_1="CHAPEL HILL" data-label_2="WEATHER" data-theme="beige" >CHAPEL HILL WEATHER</a>
+    calendar += `<a class="weatherwidget-io" href="https://forecast7.com/en/35d91n79d06/chapel-hill/?unit=us" data-label_1="CHAPEL HILL" data-label_2="WEATHER" data-days="3" data-theme="beige" >CHAPEL HILL WEATHER</a>
     <script>
     !function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src='https://weatherwidget.io/js/widget.min.js';fjs.parentNode.insertBefore(js,fjs);}}(document,'script','weatherwidget-io-js');
     </script>`;
